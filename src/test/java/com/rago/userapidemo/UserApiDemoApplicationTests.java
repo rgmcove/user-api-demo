7079104d5b702c98259a96a9466d1cfb563c0d7e
rgmcove <ralberto115@gmail.com>
2022-08-09 20:18:41 -0500

    first commit

---
+++ src/test/java/com/rago/userapidemo/UserApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rago.userapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
